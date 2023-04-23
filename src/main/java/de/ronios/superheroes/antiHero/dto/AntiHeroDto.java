58ca1dba402b0ef555ce5ac0ec4e2f365c9bf45a
roosman <dev@ronios.de>
2023-04-23 13:11:10 +0200

    create a DTO for the anti-hero entity

---
+++ src/main/java/de/ronios/superheroes/antiHero/dto/AntiHeroDto.java
@@ -0,0 +1,18 @@
+package de.ronios.superheroes.antiHero.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AntiHeroDto {
+    private UUID id;
+    @NotNull(message = "First Name is required")
+    private String firstName;
+    private String lastName;
+    private String house;
+    private String knownAs;
+}
