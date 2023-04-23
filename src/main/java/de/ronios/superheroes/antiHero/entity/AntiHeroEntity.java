ebe3247ebca5192ea0ee76f9d0e949b5044d9276
roosman <dev@ronios.de>
2023-04-23 12:51:04 +0200

    create AntiHeroEntity

---
+++ src/main/java/de/ronios/superheroes/antiHero/entity/AntiHeroEntity.java
@@ -0,0 +1,31 @@
+package de.ronios.superheroes.antiHero.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class AntiHeroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @NotNull(message = "First Name is required")
+    private String firstName;
+
+    private String lastName;
+    private String house;
+    private String knownAs;
+    private String createdAt = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z").format(new Date());
+}
