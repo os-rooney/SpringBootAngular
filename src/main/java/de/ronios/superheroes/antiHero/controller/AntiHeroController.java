8b6c7b392939a21d8afab5f6cb89c8ec89ebcbc4
roosman <dev@ronios.de>
2023-04-23 14:11:30 +0200

    two functions that will convert our entity into a DTO, or vice versa.

--- src/main/java/de/ronios/superheroes/antiHero/controller/AntiHeroController.java
+++ src/main/java/de/ronios/superheroes/antiHero/controller/AntiHeroController.java
@@ -1,5 +1,7 @@
 package de.ronios.superheroes.antiHero.controller;
 
+import de.ronios.superheroes.antiHero.dto.AntiHeroDto;
+import de.ronios.superheroes.antiHero.entity.AntiHeroEntity;
 import de.ronios.superheroes.antiHero.service.AntiHeroService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -12,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AntiHeroController {
     private final AntiHeroService service;
     private final ModelMapper mapper;
+
+    private AntiHeroDto convertToDto(AntiHeroEntity entity) {
+        return mapper.map(entity, AntiHeroDto.class);
+    }
+
+    private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
+        return mapper.map(dto, AntiHeroEntity.class);
+    }
 }
