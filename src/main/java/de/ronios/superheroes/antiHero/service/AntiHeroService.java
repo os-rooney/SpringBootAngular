e1713bf9a4477db1aa628873fb53dede73cffbe2
roosman <dev@ronios.de>
2023-04-23 13:37:52 +0200

    After creating custom exception NotFoundException, we can use it now on findAntiHeroById(), updateAntiHero() and deleteAntiHero() methods to check whether the data exists before updating or deleting it.

--- src/main/java/de/ronios/superheroes/antiHero/service/AntiHeroService.java
+++ src/main/java/de/ronios/superheroes/antiHero/service/AntiHeroService.java
@@ -1,6 +1,7 @@
 package de.ronios.superheroes.antiHero.service;
 
 import de.ronios.superheroes.antiHero.entity.AntiHeroEntity;
+import de.ronios.superheroes.antiHero.exception.NotFoundException;
 import de.ronios.superheroes.antiHero.repository.AntiHeroRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,26 @@ public class AntiHeroService {
     public Iterable<AntiHeroEntity> findAllAntiHeroes() {
         return antiHeroRepository.findAll();
     }
-    public Optional<AntiHeroEntity> findAntiHeroById(UUID id) {
-        return antiHeroRepository.findById(id);
+    public AntiHeroEntity findAntiHeroById(UUID id) {
+        return findOrThrow(id);
     }
     public void removeAntiHeroById(UUID id) {
+        findOrThrow(id);
         antiHeroRepository.deleteById(id);
     }
     public AntiHeroEntity addAntiHero(AntiHeroEntity antiHero) {
         return antiHeroRepository.save(antiHero);
     }
     public void updateAntiHero(UUID id, AntiHeroEntity antiHero) {
+        findOrThrow(id);
         antiHeroRepository.save(antiHero);
+    }
+
+    private AntiHeroEntity findOrThrow(final UUID id) {
+        return antiHeroRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new NotFoundException("Anti-hero by id " + id + " was not found")
+                );
     }
 }
