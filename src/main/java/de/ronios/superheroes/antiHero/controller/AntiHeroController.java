cfa6ffba171e89bde0c9759902e8a113a7cb316e
roosman <dev@ronios.de>
2023-04-23 14:07:05 +0200

    configuration is all done, and now, we can add AntiHeroService and ModelMapper to the controller

--- src/main/java/de/ronios/superheroes/antiHero/controller/AntiHeroController.java
+++ src/main/java/de/ronios/superheroes/antiHero/controller/AntiHeroController.java
@@ -1,6 +1,8 @@
 package de.ronios.superheroes.antiHero.controller;
 
+import de.ronios.superheroes.antiHero.service.AntiHeroService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/anti-heroes")
 public class AntiHeroController {
+    private final AntiHeroService service;
+    private final ModelMapper mapper;
 }
